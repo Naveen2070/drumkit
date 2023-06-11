a280c9265332d2a5832ee19aca0f7df4453a7970
Naveen2070 <104181086+Naveen2070@users.noreply.github.com>
2023-06-11 10:37:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+var numofdrumbut = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numofdrumbut; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    var butid = this.innerHTML;
+    sound(butid);
+    ani(butid);
+  });
+}
+
+document.addEventListener("keypress", function (event) {
+  sound(event.key);
+  ani(event.key);
+});
+
+function sound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    default:
+      alert("Not a valid key");
+      break;
+  }
+}
+
+function ani(key) {
+  var but = document.querySelector("." + key);
+  but.classList.add("pressed");
+  setTimeout(function () {
+    but.classList.remove("pressed");
+  }, 100);
+}
